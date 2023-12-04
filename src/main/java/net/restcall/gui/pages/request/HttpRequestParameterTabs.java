5d16cf053a0c8c583968e6a4bab88bf6da03d4e8
Pavlo Ilin <pavloilin979@gmail.com>
2023-12-03 20:14:09 -0800

    STORY[#186574392] http Parameters content

--- src/main/java/net/restcall/gui/pages/request/HttpRequestParameterTabs.java
+++ src/main/java/net/restcall/gui/pages/request/HttpRequestParameterTabs.java
@@ -3,10 +3,12 @@ package net.restcall.gui.pages.request;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 
+import net.restcall.gui.pages.request.tabs.QueryParameters;
+
 public class HttpRequestParameterTabs extends JTabbedPane {
 	public HttpRequestParameterTabs() {
 		super(TOP, SCROLL_TAB_LAYOUT);
-		addTab("Parameters", new JLabel("dhsoif"));
+		addTab("Parameters", new QueryParameters());
 		addTab("Auth", new JLabel("dhsoif"));
 		addTab("Headers", new JLabel("dhsoif"));
 		addTab("Body", new JLabel("dhsoif"));
