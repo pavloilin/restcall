c8a0d39e79dccd293ace6fec5ea6bb4b1bd7fea0
Pavlo Ilin <pavloilin979@gmail.com>
2024-01-03 20:24:40 -0800

    STORY[#186574396] Request Headers Contnet

---
+++ src/main/java/net/restcall/gui/pages/request/tabs/HttpHeaders.java
@@ -0,0 +1,29 @@
+package net.restcall.gui.pages.request.tabs;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class HttpHeaders extends JPanel {
+	public HttpHeaders() {
+		super(new BorderLayout(0, 0));
+
+		var header = new JLabel("Headers");
+
+		String data[][] = { { "param1", "value1", "sdfjdif" }, { "param2", "value2", "eefef?" },
+				{ "param3", "value3", "descrptione" }, { "param4", "value4", "111" } };
+					
+		String col[] = { "Name", "Value", "Description" };
+		DefaultTableModel model = new DefaultTableModel(data, col);
+		JTable table = new JTable(model);
+
+		JScrollPane scrollpane = new JScrollPane(table);
+		add(scrollpane, BorderLayout.CENTER);
+		add(header, BorderLayout.NORTH);
+	}
+
+}
