5e3a469cd97de65aafa3da923636863e643ff6dd
Pavlo Ilin <pavloilin979@gmail.com>
2024-01-06 21:29:18 -0800

    STORY[#186777103] form data and urlencoded

--- src/main/java/net/restcall/gui/pages/request/tabs/httpbodycontents/BodyFormData.java
+++ src/main/java/net/restcall/gui/pages/request/tabs/httpbodycontents/BodyFormData.java
@@ -1,7 +1,28 @@
 package net.restcall.gui.pages.request.tabs.httpbodycontents;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class BodyFormData extends JPanel {
+	public BodyFormData() {
+		super(new BorderLayout(0, 0));
 
+		var header = new JLabel("Form Data");
+
+		String data[][] = { { "param1", "value1", "sdfjdif" }, { "param2", "value2", "eefef?" },
+				{ "param3", "value3", "descrptione" }, { "param4", "value4", "111" } };
+
+		String col[] = { "Name", "Value", "Description" };
+		DefaultTableModel model = new DefaultTableModel(data, col);
+		JTable table = new JTable(model);
+
+		JScrollPane scrollpane = new JScrollPane(table);
+		add(scrollpane, BorderLayout.CENTER);
+		add(header, BorderLayout.NORTH);
+	}
 }
