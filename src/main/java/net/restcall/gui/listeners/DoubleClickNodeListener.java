4a40860f75a9da727497fcd92b467fb98545fdfe
Pavlo Ilin <pavloilin979@gmail.com>
2024-02-17 21:20:23 -0800

    STORY[#187058820] 2click

---
+++ src/main/java/net/restcall/gui/listeners/DoubleClickNodeListener.java
@@ -0,0 +1,7 @@
+package net.restcall.gui.listeners;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public interface DoubleClickNodeListener {
+	void nodeActivated(DefaultMutableTreeNode selectedNode);
+}
