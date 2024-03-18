b6c4b27882e2cfc3647dfe0ac6c233a6471458b1
Pavlo Ilin <pavloilin979@gmail.com>
2024-03-18 15:37:47 -0700

    STORY[#187254450] QPC change model

--- src/main/java/net/restcall/gui/pages/request/tabs/QueryParametersPanel.java
+++ src/main/java/net/restcall/gui/pages/request/tabs/QueryParametersPanel.java
@@ -1,28 +1,30 @@
 package net.restcall.gui.pages.request.tabs;
 
 import java.awt.BorderLayout;
+import java.util.Vector;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 
-import net.restcall.controllers.mainwindow.rightpanelcontrollers.requestdetails.tabcontrollers.QueryParamsController;
 import net.restcall.gui.listeners.UiChangeListener;
 
 public class QueryParametersPanel extends JPanel {
-	
+
 	public static final int COLUMN_COUNT = 3;
-	
+
 	private final JTable table;
 	private UiChangeListener changeListener;
-	
+
 	public QueryParametersPanel() {
 		super(new BorderLayout(0, 0));
 
-		var header = new JLabel("Query RestCall");					
-		
+		var header = new JLabel("Query RestCall");
+
 		this.table = new JTable();
 
 		JScrollPane scrollpane = new JScrollPane(table);
@@ -33,10 +35,22 @@ public class QueryParametersPanel extends JPanel {
 	public void registerChangeListener(UiChangeListener changeListener) {
 		this.changeListener = changeListener;
 	}
-	
+
 	public void installNewData(String[][] data) {
 		String col[] = { "Name", "Value", "Description" };
 		DefaultTableModel model = new DefaultTableModel(data, col);
+		model.addTableModelListener(new TableModelListener() {
+			@Override
+			public void tableChanged(TableModelEvent e) {
+				if (changeListener != null) {
+					changeListener.uiChanged();
+				}
+			}
+		});
 		table.setModel(model);
+	}
+	public Vector<Vector> getData(){
+		return ((DefaultTableModel)table.getModel()).getDataVector();
+		
 	}
 }
