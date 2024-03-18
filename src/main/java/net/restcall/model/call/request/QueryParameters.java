514f3846949388ceae4d13216867b20716fe83b4
Pavlo Ilin <pavloilin979@gmail.com>
2024-03-17 21:34:34 -0700

    STORY[#187254446] QPC update from model

--- src/main/java/net/restcall/model/call/request/QueryParameters.java
+++ src/main/java/net/restcall/model/call/request/QueryParameters.java
@@ -1,9 +1,21 @@
 package net.restcall.model.call.request;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.restcall.model.util.SelectableNamedValue;
 
 public class QueryParameters {
- private List<SelectableNamedValue> params;
+	private List<SelectableNamedValue> params = new ArrayList<>();
+
+	public void addParameter(String name, String value) {
+		SelectableNamedValue namedValue = new SelectableNamedValue(name, value);
+		params.add(namedValue);
+	}
+
+	public List<SelectableNamedValue> getParams() {
+		return params;
+	}
+	
+	
 }
